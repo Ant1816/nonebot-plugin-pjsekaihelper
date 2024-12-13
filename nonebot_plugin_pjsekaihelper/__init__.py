e171a4f44fa6cb66dbf78173761eb08c0649faf6
Lyan Guan <glz15757339816@gmail.com>
2024-12-13 15:59:06 +0800

    修改README

--- nonebot_plugin_pjsekaihelper/__init__.py
+++ nonebot_plugin_pjsekaihelper/__init__.py
@@ -30,7 +30,7 @@ __plugin_meta__ = PluginMetadata(
     homepage="https://github.com/Ant1816/nonebot-plugin-pjsekaihelper",
     extra={
             "author": "Ant1",
-            "version": "1.0.11",
+            "version": "1.0.12",
             "priority": 10,
     },
 )
