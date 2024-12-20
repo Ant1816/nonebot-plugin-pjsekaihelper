cfaff482ba0675b3d0bc2722acceafe8f7c1fd80
Lyan Guan <glz15757339816@gmail.com>
2024-12-21 00:07:45 +0800

    添加模拟抽卡功能

--- src/nonebot_plugin_pjsekaihelper/__init__.py
+++ src/nonebot_plugin_pjsekaihelper/__init__.py
@@ -1,6 +1,9 @@
 import json
 import time
 import datetime
+import os
+import random
+import requests
 from pickle import EMPTY_LIST
 
 from nonebot import require
@@ -30,19 +33,20 @@ from nonebot.adapters.onebot.v11 import GroupMessageEvent, Bot, Event, MessageEv
 SUPERUSERS = get_driver().config.superusers
 __plugin_meta__ = PluginMetadata(
     name="nonebot_plugin_pjsekaihelper",
-    description="世界计划插件，拥有组建车队、生成角色表情包等功能，持续开发中",
+    description="世界计划插件，拥有组建车队、生成角色表情包、模拟抽卡等功能，持续开发中",
     usage="发送 pjsk help 查看帮助",
     type="application",
     supported_adapters={"~onebot.v11"},
     homepage="https://github.com/Ant1816/nonebot-plugin-pjsekaihelper",
     extra={
             "author": "Ant1",
-            "version": "1.1.2",
+            "version": "1.2.0",
             "priority": 10,
     },
 )
 
 room_data = []
+STATIC_IMAGE_URL = "https://storage.sekai.best/sekai-jp-assets/character/member/"
 plugin_data_dir: Path = store.get_plugin_data_dir()
 ROOM_LIST_FILE: Path = store.get_plugin_data_file("room_list.json")
 
@@ -69,11 +73,45 @@ def load_all_room():
     return room_data
 
 
+def generate_nums():
+    Num1 = str(random.randint(1, 26))
+    Num2 = str(random.randint(1, 50))
+
+    if len(Num1) == 1:
+        Num1 = "00" + Num1
+    elif len(Num1) == 2:
+        Num1 = "0" + Num1
+    else:
+        gachasimulate.finish("出现错误 请联系管理员")
+
+    if len(Num2) == 1:
+        Num2 = "00" + Num2
+    elif len(Num2) == 2:
+        Num2 = "0" + Num2
+    else:
+        gachasimulate.finish("出现错误 请联系管理员")
+
+    normal_url = f"{STATIC_IMAGE_URL}res{Num1}_no{Num2}_rip/card_normal.png"
+    after_training_url = f"{STATIC_IMAGE_URL}res{Num1}_no{Num2}_rip/card_after_training.png"
+    r_normal = requests.get(normal_url)
+    r_after_training = requests.get(after_training_url)
+
+    if (r_normal.status_code == 200 or r_normal.status_code == 304) and (r_after_training.status_code == 200 or r_after_training.status_code == 304):
+        return normal_url, after_training_url
+    elif r_normal.status_code == 200 or r_normal.status_code == 304:
+        return normal_url, ''
+    else:
+        generate_nums()
+
+
 help_ = on_command("pjsk help", priority=10, block=True)
+
 roomcreate = on_command("建车队", aliases={"组队", "组车队"}, priority=10, block=True)
 roomnum = on_command("车队号", aliases={"房间号", "车号", "有烤吗", "有烤嘛", "ycm"}, priority=10, block=True)
 roomdelete = on_command("删车队", aliases={"删队", "删除车队"}, priority=10, block=True)
 roomreset = on_command("重置车队列表", priority=10, block=True, permission=SUPERUSER)
+
+gachasimulate = on_command("pjsk抽卡", aliases={"啤酒烧烤抽卡"}, priority=10, block=True)
 
 
 @help_.handle()
@@ -89,7 +127,9 @@ async def handle_help_message(bot: Bot, event: GroupMessageEvent):
         "Project Sekai 表情生成\n"
         "pjsk表情列表 查看所有角色表情包\n"
         "pjsk表情 进入交互创建模式\n"
-        "pjsk表情 -h 进入Shell-like创建模式"
+        "pjsk表情 -h 进入Shell-like创建模式\n\n"
+        "Project Sekai 模拟抽卡\n"
+        "pjsk抽卡 进行模拟抽卡"
     )
     await help_.send(message)
 
@@ -175,3 +215,26 @@ async def handle_room_reset(bot: Bot, event: GroupMessageEvent):
         json.dump([], file, ensure_ascii=False, indent=2)
     room_data = []
     await roomreset.finish("重置成功")
+
+
+@gachasimulate.handle()
+async def handle_gacha(bot: Bot, event: GroupMessageEvent):
+    url_tuple = generate_nums()
+
+    while True:
+        i = 0
+        try:
+            normalurl = url_tuple[0]
+            after_training_url = url_tuple[1]
+            break
+        except Exception:
+            i += 1
+            await gachasimulate.send(f"生成出现问题...重试中...{i}次")
+            generate_nums()
+
+    if after_training_url == '':
+        await gachasimulate.send(MessageSegment.image(normalurl))
+        await gachasimulate.finish("无花后卡面")
+    else:
+        await gachasimulate.send(MessageSegment.image(normalurl))
+        await gachasimulate.finish(MessageSegment.image(after_training_url))
